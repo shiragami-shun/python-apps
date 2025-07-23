efbb18fce1e207520c4535087ccf9176709ee383
shiragami-shun <shun.shiragami0904@gmail.com>
2025-07-23 10:46:46 +0900

    審査会の課題

--- 審査会.py
+++ 審査会.py
@@ -15,17 +15,21 @@ while True:
     print("時間を入力してください。(例:12:00~18:00)")
     d = input()
     times.append(d)
+    time.sleep(1)
     # 予定の時間を入力
     print("本日行う予定を入力してください。(例:買い物,勉強)")
     e = input()
     events.append(e)
+    time.sleep(1)
     # 予定の内容を入力
     if len(times) != 0:
         for i in range(len(times)):
             if i == 0:
                 print(f"{a}-{b}-{c}-{times[i]},{events[i]}")
+                time.sleep(1)
             else:
                 print(f"{times[i]},{events[i]}")
+                time.sleep(1)
     else:
         print(f"{a}-{b}-{c}-{times[0]},{events[0]}")
     # 入力された時間と予定を表示
@@ -33,6 +37,7 @@ while True:
     print("もう一度入力しますか？続ける場合はYesを終わる場合はNoを入力してください。")
     f = input()
     if f.lower() == "yes":
+        time.sleep(1)
         print("それではもう一度入力します。")
     else:
         print("それではこのプログラムを終了します。ありがとうございました！")
